79499e025b90a9e1026f46f9c1df10f18a1c4684
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-06-27 23:28:39 -0300

    feat: basic file interpreter

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
 
 try {
-    const data = fs.readFileSync('./file.ts', 'utf8');
+    const model = fs.readFileSync('./file.ts', 'utf8');
 
     if (fs.existsSync('./new_file.ts')) {
         fs.unlinkSync('./new_file.ts');
     }
 
-    fs.writeFileSync('./new_file.ts', data, error => {
+    fs.writeFileSync('./new_file.ts', model, error => {
         if (error) console.log(error);
     });
-    console.log(data);
+    const basic_model_data = model.split('{\r\n')[1].split('  }');
+    const model_data = ''.concat(basic_model_data.slice(0, basic_model_data.length - 1));
+
+    const model_items = model_data.split('\r\n');
+
+    const trimmed_model_items = model_items.map((item) => item.trim());
+
+    const model_property = trimmed_model_items.map((item) => item.slice(0, item.indexOf('  ')));
+    const model_type = trimmed_model_items.map((item) => item.slice(item.indexOf('  ') + 1).trim());
+    const trimmed_model_type = model_type.map((item) => item.slice(0, (item.indexOf('@') ?? item.length)).trim());
+
+    model_property.map((property, index) => {
+        console.log(`A propriedade ${property} é do tipo ${trimmed_model_type[index]}`)
+    })
 } catch (error) {
     console.log(error);
 }
--- new_file.ts
+++ new_file.ts
@@ -1,1 +1,19 @@
-const teste = 'eae';
+model Users {
+    User                    Int       @id(map: "IMAPRIMARYKEY") @default(autoincrement())
+    Role                    Int?      @db.TinyInt
+    Name                    String?   @db.NVarChar(50)
+    CPF                     String?   @db.NVarChar(14)
+    RG                      String?   @db.NVarChar(20)
+    UFRG                    String?   @db.Char(2)
+    Brithday                DateTime? @db.DateTime
+    Street                  String?   @db.NVarChar(60)
+    Address                 String?   @db.NVarChar(50)
+    Number                  String?   @db.NVarChar(6)
+    Complement              String?   @db.NVarChar(10)
+    District                String?   @db.NVarChar(40)
+    City_Id                 Int?
+    UF                      String?   @db.NVarChar(2)
+    Cep                     String?   @db.NVarChar(10)
+    Phone                   String?   @db.NVarChar(14)
+    Income                  Float?    @db.Money
+  }
